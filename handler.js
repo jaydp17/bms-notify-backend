be42aa5f41e15a3fe791bdd1d376607551eb1035
Jaydeep Solanki <jaydp17@gmail.com>
2019-03-09 02:00:03 +0530

    graphql hello world

--- handler.js
+++ handler.js
@@ -1,14 +1,18 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+exports.graphql = (event, context, callback) => {
+  console.log('Received event {}', JSON.stringify(event, 3));
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  console.log('Got an Invoke Request.');
+  switch (event.field) {
+    case 'hello': {
+      callback(null, 'world');
+      break;
+    }
+
+    default: {
+      callback(`Unknown field, unable to resolve ${event.field}`, null);
+      break;
+    }
+  }
 };
