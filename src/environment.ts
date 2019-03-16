a8184525dbb2e81915a3579edf2fa041a56c3d23
Jaydeep Solanki <jaydp17@gmail.com>
2019-03-16 20:13:46 +0530

    inject environment variables to the function

--- src/environment.ts
+++ src/environment.ts
@@ -3,4 +3,4 @@ export const env: string = process.env.NODE_ENV || 'development';
 export const isProd = env === 'production';
 export const isDev = env === 'development';
 
-export const dynamoTablesPrefix = `bms-notify${isProd ? '' : '-dev'}`;
+export const dynamoTablesPrefix = `bms-notify${isProd ? '' : `-${env}`}`;
