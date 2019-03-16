3a928d877a25b9f4c1f873ecbfa00a7b19025c24
Jaydeep Solanki <jaydp17@gmail.com>
2019-03-16 18:02:25 +0530

    circleci: change cache keys to have a fresh start

--- .circleci/export-serverless-stage.sh
+++ .circleci/export-serverless-stage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SERVERLESS_STAGE=""
-if [ "${CIRCLE_TAG}" == "master" ]; then
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
   export SERVERLESS_STAGE="production"
 elif [ "${CIRCLE_TAG}" == "staging" ]; then
   export SERVERLESS_STAGE="staging"
