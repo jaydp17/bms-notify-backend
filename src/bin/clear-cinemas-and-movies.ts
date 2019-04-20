20e620d5895ff134c95788b6dda81618d5f15b13
Jaydeep Solanki <jaydp17@gmail.com>
2019-04-20 21:06:42 +0530

    add a script to clear cinemas & movies

---
+++ src/bin/clear-cinemas-and-movies.ts
@@ -0,0 +1,46 @@
+import { DocumentClient } from 'aws-sdk/lib/dynamodb/document_client';
+import { dynamoClient, paginate } from '../dynamodb';
+import { Cinema } from '../models/cinemas';
+import { Movie } from '../models/movies';
+import { cinemasTable, moviesTable } from '../tables';
+
+async function main() {
+  await clearMovies();
+  await clearCinemas();
+}
+
+async function clearMovies() {
+  const scanRequest = dynamoClient.scan({ TableName: moviesTable.TableName });
+  const movies = await paginate<Movie>(scanRequest);
+  const promises = movies.map(movie => {
+    const deleteParams: DocumentClient.DeleteItemInput = {
+      TableName: moviesTable.TableName,
+      Key: {
+        regionCode: movie.regionCode,
+        groupCode: movie.groupCode,
+      },
+    };
+    return dynamoClient.delete(deleteParams).promise();
+  });
+  await Promise.all(promises);
+}
+
+async function clearCinemas() {
+  const scanRequest = dynamoClient.scan({ TableName: cinemasTable.TableName });
+  const cinemas = await paginate<Cinema>(scanRequest);
+  const promises = cinemas.map(cinema => {
+    const deleteParams: DocumentClient.DeleteItemInput = {
+      TableName: cinemasTable.TableName,
+      Key: {
+        regionCode: cinema.regionCode,
+        code: cinema.code,
+      },
+    };
+    return dynamoClient.delete(deleteParams).promise();
+  });
+  await Promise.all(promises);
+}
+
+main()
+  .then(() => console.log('done!'))
+  .catch(err => console.error('error', err));
