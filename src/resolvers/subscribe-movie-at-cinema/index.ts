f94c584b647ab19e070696721133d1b463fd2096
Jaydeep Solanki <jaydp17@gmail.com>
2019-04-20 23:14:36 +0530

    move webpush config in a separate file

--- src/resolvers/subscribe-movie-at-cinema/index.ts
+++ src/resolvers/subscribe-movie-at-cinema/index.ts
@@ -1,4 +1,5 @@
-import webpush, { PushSubscription } from 'web-push';
+import { PushSubscription } from 'web-push';
+import { getWebPush } from '../../webpush';
 
 interface Event {
   arguments: {
@@ -9,20 +10,11 @@ interface Event {
   };
 }
 
-const vapidPublicKey =
-  'BOGGgGGFu6Vtq_w_2JRqFak_3-JjtdG02lco32frBCMjat7Vg-SkGjJz5EeM3KMTxE5lC9HzYOxEu_3o4zC6xYs';
-
 export const handler = async (event: Event) => {
   console.log('event', event);
-  const vapidPrivateKey = process.env.VAPID_PRIVATE_KEY;
-  if (!vapidPrivateKey) {
-    console.log('env var VAPID_PRIVATE_KEY not passed');
-    throw new Error('env var VAPID_PRIVATE_KEY not passed');
-  }
-  webpush.setVapidDetails('https://bms-notify.jaydp.com', vapidPublicKey, vapidPrivateKey);
-
   const payload = JSON.stringify({ title: 'Push test' });
   const { subscription } = event.arguments;
+  const webpush = getWebPush();
   const result = await webpush.sendNotification(subscription, payload);
   console.log('result', result);
   return 'subscribed :)';
